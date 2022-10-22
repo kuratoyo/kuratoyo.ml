db4240442198090f805b9c8fa168d6e677740308
toyo <kuratatetoyo@gmail.com>
2022-10-22 14:10:39 +0900

    remake background

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "../styles/globals.scss";
+import "../styles/background.css";
 import "../styles/blog.scss";
 import Header from "../components/Header.jsx";
+import Background from "../components/background.jsx";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Background />
       <Header />
       <main className="container">
         <Component {...pageProps} />
