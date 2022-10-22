33e49c47bef51371b2cedad2ea64d876618aabc8
toyo <kuratatetoyo@gmail.com>
2022-10-22 14:07:11 +0900

    delete background

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import "../styles/globals.scss";
-import "../styles/background.css";
 import "../styles/blog.scss";
 import Header from "../components/Header.jsx";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Background />
       <Header />
       <main className="container">
         <Component {...pageProps} />
